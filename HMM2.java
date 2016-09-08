14c3424d635c3b1af951f11b044a7c59f30536f0
Felix <felixab@kth.se>
2016-09-08 13:42:52 +0200

    naming stuff

--- HMM2.java
+++ HMM2.java
@@ -7,40 +7,40 @@ public class HMM2 {
 
 
     public static void main(String[] args) {
-    	String file_location = "D:/Felix/Programming/Workspace/HMM2/data/sample00";
-    	boolean use_std_input = true;
+        String file_location = "D:/Felix/Programming/Workspace/HMM2/data/sample00";
+        boolean use_std_input = false;
         readAndSolve(use_std_input, file_location);
     }
     public static void readAndSolve(boolean use_std_input, String file_location){
-    	Scanner data = null;
-    	if(use_std_input){
-			data = new Scanner(System.in);
-		}else{
-			try{
-				data = new Scanner(new File(file_location));
-			}catch ( IOException e){
-				System.out.println("Could not open file.");
-				e.printStackTrace();
-				System.exit(0);
-			}
-		}
+        Scanner data = null;
+        if(use_std_input){
+            data = new Scanner(System.in);
+        }else{
+            try{
+                data = new Scanner(new File(file_location));
+            }catch ( IOException e){
+                System.out.println("Could not open file.");
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }
         Matrix a=new Matrix(data.nextLine());
         Matrix b=new Matrix(data.nextLine());
         Matrix pi=new Matrix(data.nextLine());
-        int[] o = readInitialState(data.nextLine());
+        int[] obs_seq = readObservationSeq(data.nextLine());
         data.close();
 
-        Matrix[] alpha_container = new Matrix[o.length];
-        alpha_container[0] = pi.multiplyElementwise(b.getColumn(o[0])); /*initialize alpha 1*/
-        for (int i = 1; i<o.length; i++){
-            Matrix current_b_column = b.getColumn(o[i]);
-            alpha_container[i] = alpha_container[i-1].multiply(a).multiplyElementwise(current_b_column);
+        Matrix[] alpha_container = new Matrix[obs_seq.length];
+        alpha_container[0] = pi.multiplyElementwise(b.getColumn(obs_seq[0])); /*initialize alpha 1*/
+        for (int i = 1; i<obs_seq.length; i++){
+            Matrix current_b_column = b.getColumn(obs_seq[i]);
+            alpha_container[i] = (alpha_container[i-1].multiply(a)).multiplyElementwise(current_b_column);
         }
         alpha_container[alpha_container.length-1].printSum();
     }
 
-    public static int[] readInitialState(String inStringMatrix){
-        String[] listMatrix=inStringMatrix.split(" ");
+    public static int[] readObservationSeq(String in_string_matrix){
+        String[] listMatrix=in_string_matrix.split(" ");
         int rows=Integer.valueOf(listMatrix[0]);
         int counter=1;
         int[] out_array = new int[rows];
@@ -56,14 +56,14 @@ class Matrix {
     private final float[][] matrix;
     private final int rows;
     private final int columns;
-    private final String stringMatrix;
+    private final String string_matrix;
     
-    public Matrix(String inStringMatrix){
-        String[] listMatrix=inStringMatrix.split(" ");
+    public Matrix(String in_string_matrix){
+        String[] listMatrix=in_string_matrix.split(" ");
         rows=Integer.valueOf(listMatrix[0]);
         columns=Integer.valueOf(listMatrix[1]);
         int counter=2;
-        stringMatrix=inStringMatrix;
+        string_matrix=in_string_matrix;
         matrix=new float[rows][columns];
         for(int j=0;j<rows;j++){
             for(int k=0;k<columns;k++){
@@ -73,32 +73,32 @@ class Matrix {
         }
     }
     public Matrix multiply(Matrix inMatrix){//returns Matrix*inMatrix
-      String buildString=rows+ " " + inMatrix.columns;
+      String build_string=rows+ " " + inMatrix.columns;
         for(int h=0;h<inMatrix.columns;h++){
             for(int i=0; i<rows;i++){
-            float tmpSum=0;
+            float tmp_sum=0;
                 for(int j=0;j<columns;j++){
-                    tmpSum=tmpSum+inMatrix.matrix[j][h]*matrix[i][j];
+                    tmp_sum=tmp_sum+inMatrix.matrix[j][h]*matrix[i][j];
                 }
-            buildString=buildString+" "+ tmpSum;
+            build_string=build_string+" "+ tmp_sum;
             }  
         }
-      return new Matrix(buildString);
+      return new Matrix(build_string);
     }
     public Matrix multiplyElementwise(Matrix inMatrix){
         /*Returns matrix.*inMatrix as a row vector. Both matrix and inMatrix need to be row vectors of same dimension*/
-        String buildString=rows+ " " + columns;
+        String build_string=rows+ " " + columns;
         float element_product;
         for(int j = 0; j<columns; j++){
             element_product = matrix[0][j]*inMatrix.matrix[0][j];
-            buildString=buildString+" "+ Float.toString(element_product);
+            build_string=build_string+" "+ Float.toString(element_product);
         }
-        return new Matrix(buildString);
+        return new Matrix(build_string);
 
     }
 
     public String string(){
-        return stringMatrix;
+        return string_matrix;
     }
     public void print(){
         for(float[] row: matrix){
@@ -115,10 +115,10 @@ class Matrix {
     }
     public Matrix getColumn(int column_idx){
         /*Returns the column of a given matrix at a given index as a row vector matrix*/
-        String buildString=Integer.toString(1)+ " " + Integer.toString(rows);
+        String build_string=Integer.toString(1)+ " " + Integer.toString(rows);
         for(float[] row:matrix){
-            buildString = buildString + " " +row[column_idx];
+            build_string = build_string + " " +row[column_idx];
         }
-        return new Matrix(buildString);
+        return new Matrix(build_string);
     }
 }
