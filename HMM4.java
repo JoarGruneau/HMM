d754a78020956d7608fc9c6a9a30cc61d86d13c3
Joar Gruneau <joar@gruneau.se>
2016-09-09 14:15:39 +0200

    fixed betapass and scaled to deal with underflow problems, returns almost the same as alphapass but not entierly sure how to test it properly

--- HMM4.java
+++ HMM4.java
@@ -10,7 +10,7 @@ public class HMM4 {
     	String file_location_felix = 
                 "D:/Felix/Programming/Workspace/HMM4/data/sample00_4";
         String file_location_joar="/home/joar/Documents/AI/test.txt";
-    	boolean use_std_input = true;
+    	boolean use_std_input = false;
         readAndSolve(use_std_input, file_location_joar);
     }
     public static void readAndSolve(boolean use_std_input, String file_location){
@@ -32,38 +32,12 @@ public class HMM4 {
         int[] obs_seq = readObservationSeq(data.nextLine());
         data.close();
 
-        double prob=alphaPass(a,b,pi,obs_seq);
-        System.out.println(prob);
+        AlphaPass alpha =new AlphaPass(a,b,pi,obs_seq);
+        BetaPass beta =new BetaPass(a,b,pi,obs_seq);
+        System.out.println(beta.log_probability);
+        System.out.println(alpha.log_probability);
 
     }
-
-
-    public static double alphaPass(Matrix a, Matrix b, Matrix pi, int[] obs_seq){
-        Matrix[] alpha_container = new Matrix[obs_seq.length];
-        double[] alpha_scale=new double[obs_seq.length];
-        alpha_container[0] = pi.multiplyColumn(b, obs_seq[0]); /*initialize alpha 1*/
-        alpha_scale[0]=1/alpha_container[0].sumRow(0);
-        alpha_container[0].scale(alpha_scale[0]);
-        for (int i = 1; i<obs_seq.length; i++){
-            alpha_container[i] = alpha_container[i-1].multiply(a).
-                    multiplyColumn(b, obs_seq[i]);
-            alpha_scale[i]=1/alpha_container[i].sumRow(0);
-            alpha_container[i].scale(alpha_scale[i]);
-            
-        }
-         double log_probability=0;
-            for(double item:alpha_scale){
-                log_probability=log_probability -Math.log(item);
-                
-            }
-        return Math.exp(log_probability);
-    }
-
-    public static void betaPass(Matrix a, Matrix b, Matrix pi, int[] obs_seq){
-        // To be defined. Should return Matrix[]
-    	return;
-    }
-
 
     public static double[][] readInputMatrix(String in_string_matrix){
         String[] listMatrix=in_string_matrix.split(" ");
@@ -91,6 +65,63 @@ public class HMM4 {
         return out_array;
     }
     
+}
+class BetaPass{
+    public final double log_probability;
+    public final Matrix[] beta_container;
+    
+    public BetaPass(Matrix a, Matrix b, Matrix pi, int[] obs_seq){        
+        int obs_length=obs_seq.length;
+        double[][] initial= new double[1][pi.columns];
+        double[] beta_scale=new double[obs_length];
+        Arrays.fill(initial[0], 1);
+        Matrix[] tmp_beta_container=new Matrix[obs_seq.length];
+        //initialize beta_(T-1)
+        tmp_beta_container[obs_length-1]=new Matrix(initial);
+        beta_scale[obs_length-1]=1/tmp_beta_container[obs_length-1].sumRow(0);
+        tmp_beta_container[obs_length-1].scale(beta_scale[obs_length-1]);
+        for(int i=obs_length-2;i>-1;i--){
+            tmp_beta_container[i]=tmp_beta_container[i+1].
+                    transponatMultiply(a.multiplyColumn(b,obs_seq[i+1]));
+            beta_scale[i]=1/tmp_beta_container[i].sumRow(0);
+            tmp_beta_container[i].scale(beta_scale[i]);
+        }
+        double tmp_log=0;
+        for(double item:beta_scale){
+            tmp_log=tmp_log -Math.log(item);
+                
+        }
+        log_probability=tmp_log;
+        beta_container=tmp_beta_container;
+        
+        
+        // To be defined. Should return Matrix[]
+    }
+}
+class AlphaPass{
+    public final double log_probability;
+    public final Matrix[] alpha_container;
+    public AlphaPass(Matrix a, Matrix b, Matrix pi, int[] obs_seq){
+        Matrix[] tmp_alpha_container = new Matrix[obs_seq.length];
+        double[] alpha_scale=new double[obs_seq.length];
+        tmp_alpha_container[0] = pi.multiplyColumn(b, obs_seq[0]); /*initialize alpha 1*/
+        alpha_scale[0]=1/tmp_alpha_container[0].sumRow(0);
+        tmp_alpha_container[0].scale(alpha_scale[0]);
+        for (int i = 1; i<obs_seq.length; i++){
+            tmp_alpha_container[i] = tmp_alpha_container[i-1].multiply(a).
+                    multiplyColumn(b, obs_seq[i]);
+            alpha_scale[i]=1/tmp_alpha_container[i].sumRow(0);
+            tmp_alpha_container[i].scale(alpha_scale[i]);
+            
+        }
+        double tmp_log=0;
+        for(double item:alpha_scale){
+            tmp_log=tmp_log -Math.log(item);
+                
+        }
+        log_probability=tmp_log;
+        alpha_container=tmp_alpha_container;
+    }
 }
 class Matrix {
     public final double[][] matrix;
@@ -144,9 +175,12 @@ class Matrix {
         System.out.println(sum);
     }
     public Matrix multiplyColumn(Matrix in_matrix, int in_column){
-        double[][] out_matrix=new double[1][columns];
-        for(int i = 0; i<columns; i++){
-            out_matrix[0][i] = this.matrix[0][i]*in_matrix.matrix[i][in_column];
+        double[][] out_matrix=new double[rows][columns];
+        for(int h=0; h<rows; h++){ //loops through rows in it own matrix
+            //loops through columns in its own matrix
+            for(int i = 0; i<columns; i++){
+                out_matrix[h][i] = this.matrix[h][i]*in_matrix.matrix[i][in_column];
+            }
         }
         return new Matrix(out_matrix);
     }
@@ -163,8 +197,22 @@ class Matrix {
         for(double elem:matrix[row]){
             sum += elem;
         }
-        return sum;    
+        return sum;   
     } 
+    public Matrix transponatMultiply(Matrix in_matrix){
+        //computes matrix multiplication of matrix*inmatrix_transponat.
+        double[][] out_matrix = new double[rows][in_matrix.rows];
+        for(int i=0; i<rows; i++){
+            for(int j = 0; j<in_matrix.rows; j++){
+                double tmp_sum=0;
+                for(int k=0; k<columns;k++){
+                    tmp_sum=tmp_sum+matrix[i][k]*in_matrix.matrix[j][k];
+                }
+            out_matrix[i][j] =tmp_sum;
+            }
+        }
+        return new Matrix(out_matrix);
+    }
     public void scale(double scale){
         for(int i=0; i<rows; i++){
             for(int j=0;j<columns;j++){
